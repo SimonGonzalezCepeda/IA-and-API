66d58787662699c13757ab3ae9fc7e41c9cd3132
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-04-22 17:21:00 +0200

    Modified the propierties to protected.

--- app/IA/Character.php
+++ app/IA/Character.php
@@ -17,146 +17,363 @@ abstract class Character
      * relationship or to distinguish this Character for one with the same surname.
      * @var string
      */
-    private $name;
+    protected $name;
     /** Surname
      * This will be the surname of the current Character, it won't be able to be changed in teh game by the user,
      * we must remember that in the game we call first the surname, and its the common way to talk to them.
      * @var string
      */
-    private $surname;
+    protected $surname;
     /** A Hang Up
      * A Hang Up its one important feature that we can play later in the game to make more content, it is usually
      * something physic and some times mental.
      * @var string
      */
-    private $aHangUP;
+    protected $aHangUP;
     /** Physique
      * This parameter its more useful for making the game easy to drive, we will use it to simplify the relationships
      * and other stuff.
      * @var string
      */
-    private $physique;
+    protected $physique;
     /** Style
      * It defines the style of the character, and it will be easier to use this to make relationships easier. A style
      * could be a "gamer" or a "heavy".
      * @var
      */
-    private $style;
+    protected $style;
     /** Default Image
      * Its the normal pose that will be displayed with the character, everyone has its own.
      * @var
      */
-    private $defaultImage;
+    protected $defaultImage;
     /** Poses
      * This arrays will gathered all the current poses of the characters. The number will be fixed
      * TODO: For this pre-Alpha it will be 4
      * @var array
      */
-    private $poses;
+    protected $poses;
     /** Egoist
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $egoist;
+    protected $egoist;
     /** Altruist
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $altruist;
+    protected $altruist;
     /** Superficial
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $superficial;
+    protected $superficial;
     /** Romantic
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $romantic;
+    protected $romantic;
     /** Sweet
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $sweet;
+    protected $sweet;
     /** Rude
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $rude;
+    protected $rude;
     /** Crazy
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $crazy;
+    protected $crazy;
     /** Tranquil
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $tranquil;
+    protected $tranquil;
     /** Simple
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $simple;
+    protected $simple;
     /** Complex
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $complex;
+    protected $complex;
     /** Funny & Boring
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $funny8boring;
+    protected $funny8boring;
     /** Positive & Negative
-     * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
+     * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 200.
      * @var int
      */
-    private $positive8negative;
+    protected $positive8negative;
     /** Rich & Poor
-     * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
+     * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 200.
      * @var int
      */
-    private $rich8poor;
+    protected $rich8poor;
     /** Complexed
-     * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
+     * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 200.
      * @var int
      */
-    private $complexed;
-    /** Comic
-     * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
-     * @var int
-     */
-    private $comic;
+    protected $comic;
     /** Intelligence
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $intelligence;
+    protected $intelligence;
     /** Proud
      * This will be a measure to calculate compatibility with each character. Its measurable from 1 to 100.
      * @var int
      */
-    private $proud;
+    protected $proud;
     /** King
      * This will mark if this character its a King. Remember, Kings are characters associated with one or more
      * characters.
      * @var boolean
      */
-    private $king;
+    protected $king;
     /** Pawn
      * This will mark if the character follow a King.
      * @var boolean
      */
-    private $pawn;
+    protected $pawn;
     /** Height
      * Height is needed for narrative and some events.
      * @var float
      */
-    private $height;
+    protected $height;
     /** Weight
      * Weight is needed for narrative and some events.
      * @var float
      */
-    private $weight;
+    protected $weight;
+
+    // Setters
+
+
+
+    // Getters
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAHangUP()
+    {
+        return $this->aHangUP;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhysique()
+    {
+        return $this->physique;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStyle()
+    {
+        return $this->style;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDefaultImage()
+    {
+        return $this->defaultImage;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPoses()
+    {
+        return $this->poses;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEgoist()
+    {
+        return $this->egoist;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAltruist()
+    {
+        return $this->altruist;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSuperficial()
+    {
+        return $this->superficial;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRomantic()
+    {
+        return $this->romantic;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSweet()
+    {
+        return $this->sweet;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRude()
+    {
+        return $this->rude;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCrazy()
+    {
+        return $this->crazy;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTranquil()
+    {
+        return $this->tranquil;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSimple()
+    {
+        return $this->simple;
+    }
+
+    /**
+     * @return int
+     */
+    public function getComplex()
+    {
+        return $this->complex;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFunny8boring()
+    {
+        return $this->funny8boring;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPositive8negative()
+    {
+        return $this->positive8negative;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRich8poor()
+    {
+        return $this->rich8poor;
+    }
+
+    /**
+     * @return int
+     */
+    public function getComic()
+    {
+        return $this->comic;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIntelligence()
+    {
+        return $this->intelligence;
+    }
+
+    /**
+     * @return int
+     */
+    public function getProud()
+    {
+        return $this->proud;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isKing()
+    {
+        return $this->king;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isPawn()
+    {
+        return $this->pawn;
+    }
+
+    /**
+     * @return float
+     */
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    /**
+     * @return float
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
 }
