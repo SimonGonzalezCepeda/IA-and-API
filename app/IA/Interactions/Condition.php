16db74fc350c0a2cb0c201db2a4909526c187d95
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-05-05 20:25:22 +0200

    Adding Condition class in order to clarify code. I don't know yet how to do the constructor but I'm working on it.

---
+++ app/IA/Interactions/Condition.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App;
+
+
+/**
+ * Class Condition
+ * @package App
+ */
+class Condition
+{
+    /**
+     * This variable can be almost any parameter of the game, it could be the day, a week
+     * in particular, a status from the player or the relationship of both Characters.
+     * @var
+     */
+    private $condition;
+    /**
+     * This is the actual value that you will ask for it to something to start() or to end($condition).
+     * Remember that start() and end($condition) function its in almost every "event"
+     * (it can be a dialog, a conversation or an actual event) that has a
+     * listener.
+     * @var
+     */
+    private $value;
+
+    public function One($condition, $value)
+    {
+        $oneCondition = new Condition;
+        $this->value = $value;
+        $this->condition = $condition;
+        return $oneCondition;
+    }
+
+    public function Multiple()
+    {
+        $this->condition = [];
+    }
+}
