eec4ea8bf9fc97157b791fb9cc4666a6c81fbce3
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-05-11 20:41:02 +0200

    Finished class, at least at the moment.

--- app/IA/Interactions/Condition.php
+++ app/IA/Interactions/Condition.php
@@ -5,6 +5,7 @@ namespace App;
 
 
 /**
+ * This class exist only to make condition for listeners and events.
  * Class Condition
  * @package App
  */
@@ -13,40 +14,61 @@ class Condition implements Startable
     /**
      * This variable can be almost any parameter of the game, it could be the day, a week
      * in particular, a status from the player or the relationship of both Characters.
+     * This parameter must be one taken from the actual object comparative.
      * @var
      */
-    private $condition;
+    private $comparator;
     /**
      * This is the actual value that you will ask for it to something to start()
      * or to end($condition). Remember that start() and end($condition) function
      * its in almost every "event" (it can be a dialog, a conversation or an
      * actual event) that has a listener.
-     * @var
+     * @var mixed
      */
     private $value;
 
-    public function One($condition, $value)
+    /**
+     * This constructor has been made to make one Simple condition.
+     * @param $comparator
+     * @param $value
+     * @return Condition
+     */
+    public function Simple($comparator, $value)
     {
-        $oneCondition = new Condition;
-        $this->value = $value;
-        $this->condition = $condition;
-        return $oneCondition;
-    }
-
-    public function Multiple()
-    {
-        $this->condition = [];
+        $simpleCondition = new Condition;
+        $simpleCondition->value = $value;
+        $simpleCondition->comparator = $comparator;
+        return $simpleCondition;
     }
 
     /**
+     * This constructor has been made to make multiple conditions and put it into
+     * one object.
+     * @param array $comparator
+     * @param array $value
+     * @return Condition
+     */
+    public function Multiple(array $comparator, array $value)
+    {
+        $multipleCondition = new Condition;
+        $multipleCondition->comparator = $comparator;
+        $multipleCondition->value = $value;
+        return $multipleCondition;
+    }
+
+    /**
+     * Return the value to compare with, this value its the actual value of
+     * an object.
      * @return mixed
      */
-    public function getCondition()
+    public function getComparator()
     {
-        return $this->condition;
+        return $this->comparator;
     }
 
     /**
+     * Return the value that we wish to achive so the current condition can be
+     * acomplished.
      * @return mixed
      */
     public function getValue()
